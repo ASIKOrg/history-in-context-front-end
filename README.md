bcc5588b58f204da47c67247432ccd7853382822
Ian Bantoto <lcbantoto@gmail.com>
2015-02-22 13:36:04 -0500

    did stuff

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # history-in-context-front-end
+
+Take in input from search box
+Pass in as GET param text
+
+aClient = new HttpClient();
+aClient.get('http://some/thing?=arguments', function(answer) {
+    // do something with answer
+});
--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 // Array of all json date objects
 var events[];
+var inQuery;
+var outQuery;
 
-document.getElementById("hello").push("eh");
+function 
 
-// Interact with backend
-function search(){
-    var text = '{"start":1999,"end":2015,"description":"Stuart Lives"}';
+function buttonClicked(){
+    inQuery = $('#search').val();
+    outQuery = httpGet(inQuery);
+    showResult();
+}
 
+// 
+function showResult(){
+    
+    var text = '{"start":1999,"end":2015,"description":' + query + '"}';
+    
     var obj = JSON.parse(text);
-
-    document.getElementById("hello").innerHTML =
-    obj.start + " - " +
-    obj.end + "<br>" +
-    obj.description;
+    
+    document.getElementById("event").innerHTML = 
+        obj.start + " - " +
+        obj.end + "<br>" +
+        obj.description;
     display();
     events[events.length] = obj;
     display();
@@ -20,18 +29,27 @@ function search(){
 
 // Display each of the objects
 function display(){
+    var wrapper = $('#timeline_box');
     var empty = true;
     var minYear = -9999;
-    for(var b: event)
+    for(var event: events){
+        for(var key in event){
+            item = $('<div id="event" class="event"></div>');
+            wrapper.append(item);
+            item.append('<div class="description">' + events[key].description +'</div>');
+            item.append('<div class="start">' + events[key].start +'</div>');
+            item.append('<div class="end">' + events[key].end +'</div>');
+        }
+    }
 }
 
 // Make GET Request
-function httpGet(theUrl)
+function httpGet(search)
 {
     var xmlHttp = null;
     
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.open( "GET", '/api/?=' + search, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
