96e4409b97d006f0053844b75bab7fce85c80ba3
Ian Bantoto <lcbantoto@gmail.com>
2015-02-22 12:16:54 -0500

    Fix Indents, Comments

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
-//array of all json date objects
-        var events[];
+// Array of all json date objects
+var events[];
 
-        document.getElementById("hello").push("eh");
+document.getElementById("hello").push("eh");
 
-        function search(){
-            var text = '{"start":1999,"end":2015,"description":"Stuart Lives"}';
+// Interact with backend
+function search(){
+    var text = '{"start":1999,"end":2015,"description":"Stuart Lives"}';
 
-            var obj = JSON.parse(text);
+    var obj = JSON.parse(text);
 
-            document.getElementById("hello").innerHTML =
-            obj.start + " - " +
-            obj.end + "<br>" +
-            obj.description;
-            display();
-            events[events.length] = obj;
-            display();
-        }
+    document.getElementById("hello").innerHTML =
+    obj.start + " - " +
+    obj.end + "<br>" +
+    obj.description;
+    display();
+    events[events.length] = obj;
+    display();
+}
 
-        //displays each of the objects
-        function display(){
-            var empty = true;
-            var minYear = -9999;
-            for(var b: event)
-        }
+// Display each of the objects
+function display(){
+    var empty = true;
+    var minYear = -9999;
+    for(var b: event)
+}
 
-        function httpGet(theUrl)
-        {
-            var xmlHttp = null;
-
-            xmlHttp = new XMLHttpRequest();
-            xmlHttp.open( "GET", theUrl, false );
-            xmlHttp.send( null );
-            return xmlHttp.responseText;
-        }
+// Make GET Request
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+    
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
